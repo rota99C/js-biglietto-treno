f6bdc7058d0de063abf39fa62b61855107dacfcd
rota99C <rotaboolean@gmail.com>
2021-10-11 17:39:02 +0200

    sconto over 65 completo

--- main.js
+++ main.js
@@ -4,18 +4,29 @@ const userAge = prompt("Quanti anni hai?")
 
 //calcolo costo della tratta 
 let costoTratta = parseInt(userKilometer) * 0.21 
-console.log(costoTratta);
 
 //calcolo sconto 20% 
 let scontoMinorenni = costoTratta * 20 / 100
 console.log(scontoMinorenni);
 
+//calcolo sconto 40%
+let scontoOver = costoTratta * 40 / 100
+console.log(scontoOver);
 
 
 //applicazione dello sconto a seconda dell'età 
 
 if (userAge < 18) {
     console.log(costoTratta - scontoMinorenni);
-
-
 }
+
+else if (userAge > 65) {
+    console.log(costoTratta - scontoOver);
+}
+
+else if (userAge >= 18 && userAge <= 65) {
+    console.log(costoTratta);
+}
+
+
+
