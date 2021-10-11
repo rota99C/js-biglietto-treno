e94cbbbd179c0cb4cc5dc8b192087917f4d42a2b
rota99C <rotaboolean@gmail.com>
2021-10-11 17:58:54 +0200

    risultato completo anche sul html

--- main.js
+++ main.js
@@ -9,24 +9,33 @@ let costoTratta = parseInt(userKilometer) * 0.21
 let scontoMinorenni = costoTratta * 20 / 100
 console.log(scontoMinorenni);
 
+const priceMinorenni = costoTratta - scontoMinorenni
+
 //calcolo sconto 40%
 let scontoOver = costoTratta * 40 / 100
 console.log(scontoOver);
+
+const priceOver = costoTratta - scontoOver
+
+
 
 
 //applicazione dello sconto a seconda dell'età 
 
 if (userAge < 18) {
-    console.log(costoTratta - scontoMinorenni);
+    document.getElementById("risultato"). innerHTML = `il costo del tuo biglietto è ${priceMinorenni} €`
 }
 
 else if (userAge > 65) {
-    console.log(costoTratta - scontoOver);
+    document.getElementById("risultato"). innerHTML = `il costo del tuo biglietto è ${priceOver} €`
+    
 }
 
 else if (userAge >= 18 && userAge <= 65) {
-    console.log(costoTratta);
+    document.getElementById("risultato"). innerHTML = `il costo del tuo biglietto è ${costoTratta} €`
 }
+
+
 
 
 
